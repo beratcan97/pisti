3402f02b2da0d7eae015ef8ce5f7ec413a24035b
unknown <beratcan_Acil@hotmail.se>
2018-06-19 20:33:36 +0200

    change of computer

--- script.js
+++ script.js
@@ -4,20 +4,52 @@ var playerCards = [];
 var robotCards = [];
 
 for (i = 0; i < 4; i++) {
-    playerCards.push(Math.floor(Math.random() * 52) + 1);
-    robotCards.push(Math.floor(Math.random() * 52) + 1);
+    // Gives player cards
+    playerCards.push(getCard());
+
+
+    //Gives robot cars
+    robotCards.push(getCard());
 }
 
-// removes card
-function remove(removeCard){
-    for(var card of cards){
-        if(card = removeCard){
-            alert("same");
+// checks if the card is left
+function getCard(){
+    // Generates a rundom number between 1 and 52
+    function randomCard(){
+        var randomCard = Math.floor(Math.random() * 52) + 1;
+
+        // Tests if the card is left
+        for(var card of cards){
+            if(card == randomCard){
+                return randomCard;
+            }
         }
+    }
+    
+    var randomCardResult = randomCard();
+
+    function testCard() {
+        if(randomCardResult == null){
+            //If the choosen card was not left runs the code again
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    var randomCardTestResult = testCard();
+
+    if(testCard() == false) {
+        /*
+        while(testCard() == false) {
+            randomCard();
+            testCard();
+        }
+        */
+        return randomCardResult;
     }
 }
 
 console.log(playerCards);
 console.log(robotCards);
-
-
