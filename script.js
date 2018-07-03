a5e76f93dd5261caacf678a99030906dae09afb6
beratcan97 <beratcan_acil@hotmail.se>
2018-07-04 01:11:16 +0200

    big update

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 var cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52"];
+var playerPoints = 0;
 
 var middleCards = [];
 var playerCards = [];
 var robotCards = [];
+//Create a function that finds the topcardinmiddle
+var topCardInMiddle = [];
+
 var playerTurn = true;
-var playerPoints = 0;
-var robotPoints = 0;
-var playerCardsInPocket = 0;
-var robotCardsInPocket = 0;
 
-
-//Starts the game
-middleCards = [39, 1, 50, 34];
-//middleCards = randomCard(4);
+middleCards = randomCard(4);
 playerCards = randomCard(4);
 robotCards = randomCard(4);
 
@@ -29,117 +26,101 @@ function randomCard(number) {
     return randomCards;
 }
 
-//Display the cards
-function updateCards(){
-    for(var card of robotCards){
-        createCards(card, "robotCards");
-    }
-    for(var card of middleCards){
-        createCards(card, "middleCards");
-    }
-    for(var card of playerCards){
-        createCards(card, "playerCards");
-    }
-}
-updateCards();
+function robotPlays(){
+    //robot random card
+    var rndCard = Math.floor(Math.random() * robotCards.length);
 
-//Create cards
-function createCards(card, elementID){
-    var pTag = document.createElement("p");
-    var pTagText = document.createTextNode(card);
-    pTag.appendChild(pTagText);
+    //adds the card to middle
+    middleCards.push(robotCards[rndCard]);
 
-    pTag.classList.add(elementID);
+    //remove random card from robot cards
+    robotCards.splice(rndCard, 1);
 
-    var element = document.getElementById(elementID);
-    element.appendChild(pTag);
-}
-
-//Remove card           //NOT IN USE RIGHT NOW
-function removeCard(cardToRemove) {
-    elm = document.getElementById("playerCards");
-    elm.removeChild(elm.childNodes[cardToRemove]);
-}
-
-//Adds onclick funtion
-function updateClassList() {
-    return userSelection = document.getElementsByClassName('playerCards');
-}
-updateClassList();
-
-for(let i = 0; i < userSelection.length; i++) {
-userSelection[i].addEventListener("click", function() {
-    if(playerTurn) {
-        playerPlays(userSelection[i].innerHTML);
-        //TEST
-        userSelection[i].classList.toggle('hidden');
+    //gives robot new card if there is non card left in hand
+    if(robotCards == 0){
+        robotCards = randomCard(4);
     }
-    else {
-        alert("Not your turn!");
-    }
-})
-}
 
-//Count points
-
-//In test ---- Some thing wrong here only the last 2 if does not work
-function countPoints(card) {
-    alert(card);
-    //10 of diamonds is worth 3 point
-    if (card == 34) {
-        return 3;
-    }
-    //2 of clubs is worth 2 point
-    if (card == 1) {
-        return 2;
-    }
-    //All the J is worth 1 point
-    if ((card == 37) || (card == 38) || (card == 39) || (card == 40)){
-        return 1;
-    }
-    //All the Aces is worth 1 point
-    if ((card == 49) || (card == 50) || (card == 51) || (card == 52)){
-        return 1;
-    }
-    else {
-        var card = card;
-    }
-}
-
-//Game  logic
-function gameLogic(userCard) {
-    for(var card of middleCards){
-        var resultForCard = countPoints(card);
-        if(resultForCard ==! null){
-            alert("points to be added: " + resultForCard);
-            playerPoints += resultForCard;
-            alert("Result: " + resultForCard);
-        }
-    }
-}
-
-//player plays
-function playerPlays(card){
-    gameLogic(card);
-    playerTurn = false;
-}
-
-//computerplayer logic
-function computerPlays(){
-    alert("computers turn");
-    var randomCard = robotCards[Math.floor(Math.random()*robotCards.length)];
-    alert(randomCard);
-    //gameLogic();
+    //gives turn to player
+    alert("Your turn");
     playerTurn = true;
-    alert("computer have played");
 }
 
+function playerPlays(cardIndex) {
+    var cardValue = playerCards[cardIndex];
+    if(playerTurn == true){
+        //add the card to middlecards
+        middleCards.push(playerCards[cardIndex]);
 
-//The most card
-function playerWithMostCard(player){
-    if (playerCardsInPocket > robotCardsInPocket) {
-    }
-    else if (playerCardsInPocket < robotCardsInPocket){
-        robotPoints +3;
+        //removes card from playerCards[]
+        playerCards.splice(cardIndex, 1);
+
+        //if ther is no card left in the hand then 4 new cards will be generated
+        if(playerCards == 0){
+            playerCards = randomCard(4);
+        }
+
+        //if the card is J then empty the middlecards and count the points
+        if (((cardValue == 37) || (cardValue == 38) || (cardValue == 39) || (cardValue == 40)) || (cardValue == topCardInMiddle)){
+            //counts the points
+            for(var card of middleCards){
+                //10 of diamonds is worth 3 point
+                if ((card == 34)) {
+                    playerPoints += 3;
+                }
+                //2 of clubs is worth 2 point
+                if ((card == 1)) {
+                    playerPoints += 2;
+                }
+                //All the J is worth 1 point
+                if ((card == 37) || (card == 38) || (card == 39) || (card == 40)){
+                    playerPoints += 1;
+                }
+                //All the Aces is worth 1 point
+                if ((card == 49) || (card == 50) || (card == 51) || (card == 52)){
+                    playerPoints += 1;
+                }
+            }
+            //empty the middle cards
+            middleCards.splice(0, middleCards.length);
+        }
+        //visual unpdate
+        updateCards();
+
+        //Gives the turn to robot
+        playerTurn = false;
+        robotPlays();
     }
 }
+
+function updateCards(){
+    var counter = 0;
+    for(var card of playerCards){
+        document.getElementById("playerCard" + (counter + 1)).innerHTML = card;
+        counter ++;
+    }
+
+    var backCounter = 4;
+    for (let i = 0; i < (4 - counter); i++) {
+        document.getElementById("playerCard" + backCounter).innerHTML = "x";
+        backCounter--;
+    }
+
+    console.log(playerCards);
+}
+
+function cardElements() {
+
+    updateCards();
+
+    document.getElementById("playerCard1").addEventListener("click", function(){playerPlays(0)});
+    document.getElementById("playerCard2").addEventListener("click", function(){playerPlays(1)});
+    document.getElementById("playerCard3").addEventListener("click", function(){playerPlays(2)});
+    document.getElementById("playerCard4").addEventListener("click", function(){playerPlays(3)});
+}
+
+//Start the game
+function gameStart(){
+    cardElements();
+}
+gameStart();
