c87cfd5c0c407907745ccbeebd6e9a4f5fc7eaef
beratcan97 <beratcan_acil@hotmail.se>
2018-07-12 22:12:45 +0200

    big changes almost working

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52"];
 var playerPoints = 0;
+var robotPoints = 0;
+
+var winnerOfRound = "";
 
 var middleCards = [];
 var playerCards = [];
@@ -30,19 +33,52 @@ function robotPlays(){
     //robot random card
     var rndCard = Math.floor(Math.random() * robotCards.length);
 
+    var cardValue = robotCards[rndCard];
+
     //adds the card to middle
-    middleCards.push(robotCards[rndCard]);
+    middleCards.push(cardValue);
 
     //remove random card from robot cards
     robotCards.splice(rndCard, 1);
 
-    //gives robot new card if there is non card left in hand
+    //if ther is no card left in the hand then 4 new cards will be generated
     if(robotCards == 0){
         robotCards = randomCard(4);
     }
 
+    //if the card is J then empty the middlecards and count the points
+    if (((cardValue == 37) || (cardValue == 38) || (cardValue == 39) || (cardValue == 40)) || (cardValue == topCardInMiddle)){
+        //counts the points
+        for(var card of middleCards){
+            //10 of diamonds is worth 3 point
+            if ((card == 34)) {
+                robotPoints += 3;
+            }
+            //2 of clubs is worth 2 point
+            if ((card == 1)) {
+                robotPoints += 2;
+            }
+            //All the J is worth 1 point
+            if ((card == 37) || (card == 38) || (card == 39) || (card == 40)){
+                robotPoints += 1;
+            }
+            //All the Aces is worth 1 point
+            if ((card == 49) || (card == 50) || (card == 51) || (card == 52)){
+                robotPoints += 1;
+            }
+            //-------------------------------------------------------------------------------------------------------------------------------------------
+            winnerOfRound = "Robot";
+        }
+
+        //empty the middle cards
+        middleCards.splice(0, middleCards.length);
+    }
+
+
+    updateMiddleCards();
+
+
     //gives turn to player
-    alert("Your turn");
     playerTurn = true;
 }
 
@@ -83,9 +119,11 @@ function playerPlays(cardIndex) {
             }
             //empty the middle cards
             middleCards.splice(0, middleCards.length);
+
+            winnerOfRound = "Player";
         }
-        //visual unpdate
-        updateCards();
+        //visual update
+        visualUpdate();
 
         //Gives the turn to robot
         playerTurn = false;
@@ -93,7 +131,8 @@ function playerPlays(cardIndex) {
     }
 }
 
-function updateCards(){
+function visualUpdate(){
+    //Updates visual player cards
     var counter = 0;
     for(var card of playerCards){
         document.getElementById("playerCard" + (counter + 1)).innerHTML = card;
@@ -105,13 +144,24 @@ function updateCards(){
         document.getElementById("playerCard" + backCounter).innerHTML = "x";
         backCounter--;
     }
+    
+    //Updates visual player points
+    document.getElementById("playerPoints"). innerHTML = ("Points: " + playerPoints);
+}
 
-    console.log(playerCards);
+function updateMiddleCards(){
+    if(middleCards[0] == null){
+        document.getElementById("middleCard").innerHTML = (winnerOfRound + " wins the round!");
+    }
+    else {
+        document.getElementById("middleCard").innerHTML = middleCards.slice(-1)[0];
+    }
 }
 
 function cardElements() {
 
-    updateCards();
+    visualUpdate();
+    updateMiddleCards();
 
     document.getElementById("playerCard1").addEventListener("click", function(){playerPlays(0)});
     document.getElementById("playerCard2").addEventListener("click", function(){playerPlays(1)});
